e84f14179b41419017be260e6063565d9a9e8801
vail131 <vail131@naver.com>
2023-02-13 23:00:28 +0900

    [#1] fix model codes with dimension

--- image_classification/models/image_cls_model.py
+++ image_classification/models/image_cls_model.py
@@ -6,26 +6,22 @@ class ImageClassificationModel(nn.Module):
         self,
         num_labels: int,
     ):
-        super(ImageClassificationModel).__init__()
+        super().__init__()
         # input shape(256, 256, 3)
-        # conv shape(11x11, stride 5)
-        # conv output(50, 50, 64)
-        # max pool shape(4x4, 3, stride 2)
-        # max pool output(24, 24, 64)
+        # conv shape(11x11, stride 5) -> conv output(50, 50, 64)
+        # max pool shape(4x4, 3, stride 2) -> max pool output(24, 24, 64)
         self.conv_layer1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=5, stride=3),
-            nn.BatchNorm2d(),
+            nn.Conv2d(3, 64, kernel_size=11, stride=5),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=4, stride=2),
         )
         # input shape(24, 24, 64)
-        # conv shape(5x5, stride 1)
-        # conv output(20, 20, 256)
-        # max pool shape(4x4, stride 2)
-        # max pool output(9, 9, 256)
+        # conv shape(5x5, stride 1) -> conv output(20, 20, 256)
+        # max pool shape(4x4, stride 2) -> max pool output(9, 9, 256)
         self.conv_layer2 = nn.Sequential(
-            nn.Conv2d(64, 256, kernel_size=3, stride=1),
-            nn.BatchNorm2d(),
+            nn.Conv2d(64, 256, kernel_size=5, stride=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=4, stride=2),
         )
@@ -33,9 +29,27 @@ class ImageClassificationModel(nn.Module):
         self.fc_layer = nn.Linear(9 * 9 * 256, num_labels)
 
     def forward(self, x):
-        output = self.conv_layer1(x)
-        output = self.conv_layer2(output)
-        output = nn.Flatten()(output)
-        output = self.fc_layer(output)
+        try:
+            output = self.conv_layer1(x)
+            output = self.conv_layer2(output)
+            output = nn.Flatten()(output)
+            output = self.fc_layer(output)
+        except Exception as e:
+            print(f"error : {e}")
 
         return output
+
+
+if __name__ == "__main__":
+    import torch
+
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+    model = ImageClassificationModel(num_labels=10)
+    model.to(device)
+
+    dummy_data = torch.rand([1, 3, 256, 256])
+    dummy_data = dummy_data.to(device)
+
+    output = model(dummy_data)
+    print(output)
