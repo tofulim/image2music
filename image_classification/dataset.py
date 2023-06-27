54e625f5119be93b9ab731760397ce936f3d161b
vail131 <vail131@naver.com>
2023-06-28 00:15:14 +0900

    [#1] add date shuffle func

--- image_classification/dataset.py
+++ image_classification/dataset.py
@@ -1,3 +1,4 @@
+import random
 import urllib.request
 from io import BytesIO
 
@@ -14,11 +15,13 @@ class ImageDataset(Dataset):
     """
 
     def __init__(self, data_path: str):
+        # 이미지 전처리 프로세서 로드
         self.processor = self._get_processor()
-        data = pd.read_csv(data_path)
+        # TODO delete iloc
+        data = pd.read_csv(data_path).iloc[:10]
 
-        self.images, self.labels = list(), list()
-
+        self.images, self.labels = [], []
+        # pd.DataFrame에 있는 image url 주소로 요청을 보내 이미지를 받아 저장한다.
         for vid_label, image_url in tqdm(data[["vid_label", "thumbnail_url"]].values):
             req = urllib.request.Request(
                 image_url, headers={"User-Agent": "Mozilla/5.0"}
@@ -27,6 +30,27 @@ class ImageDataset(Dataset):
             input_image = Image.open(BytesIO(res))
             self.images.append(self.processor(input_image))
             self.labels.append(vid_label)
+
+        print(f"images: {len(self.images)}")
+        print(f"labels: {self.labels}")
+        self.images, self.labels = self._shuffle_items(self.images, self.labels)
+
+    def _shuffle_items(self, *args):
+        """
+        주어진 데이터, 라벨 쌍을 섞어 반환한다.
+
+        Args:
+            *args (list): 섞어줄 images(list), labels(list)
+
+        Returns:
+            (images, labels)
+
+        """
+        items = list(zip(*args))
+        random.shuffle(items)
+        shuffled_items = list(zip(items))
+
+        return shuffled_items[0], shuffled_items[1]
 
     def __len__(self):
         return len(self.labels)
@@ -54,7 +78,7 @@ class ImageDataset(Dataset):
 if __name__ == "__main__":
     from torch.utils.data import DataLoader
 
-    train_dataset = ImageDataset(data_path="data/youtube_data.csv")
+    train_dataset = ImageDataset(data_path="image_classification/data/valid_data.csv")
     train_dataloader = DataLoader(
         dataset=train_dataset,
         shuffle=True,
